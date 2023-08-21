ed59e4d7c527df1427fea2a0f6b90e2e1f2e6aeb
jeevaa <balag97@gmail.com>
2023-08-21 05:37:32 +0000

    pushing the scripts

---
+++ 2_firstscript.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#Variable Declartion
+PACKAGE="httpd wget unzip"
+SVC="httpd"
+URL="https://www.tooplate.com/zip-templates/2136_kool_form_pack.zip"
+ART_NAME="2136_kool_form_pack"
+TEMPDIR="/tmp/webfiles"
+
+#Installing Packages
+echo "#######################################"
+echo "INSTALLING PACKAGES"
+echo "#######################################"
+sudo yum install -y $PACKAGE
+echo
+
+#START & ENABLE THE SERVICE
+echo "########################################"
+echo "START & ENABLE THE HTTPD SERVICE"
+echo "########################################"
+sudo systemctl start $SVC
+sudo systemctl enable $SVC
+sudo systemctl status $SVC
+
+echo "####################################"
+# Creating Temporary Directory
+echo "###################################"
+# Starting artifact deployment
+mkdir -p $TEMPDIR
+cd $TEMPDIR
+
+
+
+wget $URL
+unzip $ART_NAME.zip
+sudo cp -r $TEMPDIR/$ART_NAME/* /var/www/html/
+echo "###################################"
+echo "REBOOT SERVICES"
+echo "####################################"
+sudo systemctl restart $SVC
+
+rm -rf $TEMPDIR
+ls /var/www/html
+
+
